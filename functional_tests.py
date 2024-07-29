bc566e6d6252c16f509ed8cc7d139eb539b19914
akduang <2019364641@qq.com>
2024-07-29 11:49:25 +0800

    update

--- functional_tests.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver # type: ignore
-brower = webdriver.Firefox()
-
-brower.get('http://localhost:8000')
-# print(brower.title)
-# assert 'django' in brower.title
-assert 'The install worked successfully! Congratulations!' in brower.title
