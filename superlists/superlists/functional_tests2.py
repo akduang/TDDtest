0731f5677d22589207baaff74fdfb7df02410a09
akduang <2019364641@qq.com>
2024-08-02 11:29:44 +0800

    next make

--- superlists/superlists/functional_tests2.py
+++ superlists/superlists/functional_tests2.py
@@ -9,6 +9,11 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
         #伊迪斯听说有一个很酷的在线待办事项应用
         #她去看了这个应用的首页
@@ -36,13 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         #待办事项表格中显示了“1：Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table.Contents were:\n{table.text}"
-        )
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
         #页面中又显示了一个文本框，可以输入其他的待办事项
         #她输入了“Use peacock feathers to make a fly”
         #伊迪斯做事很有条理
@@ -51,13 +50,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         #页面再次更新，她的清单中显示了这两个待办事项
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2:Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
         #伊迪斯想知道这个网站是否会记住她的清单
         #她看到网站为她生成了一个唯一的URL
         #而且页面有一些文字解说这个功能
