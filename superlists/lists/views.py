547253327717297eda6ef0564cdca3a1c77deeda
akduang <2019364641@qq.com>
2024-08-02 11:57:19 +0800

    before pass remake

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lists.models import Item
 # Create your views here.
 # home_page = None
 
@@ -7,6 +8,9 @@ def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
     # if request.method == 'POST':
     #     return HttpResponse(request.POST['item_text'])
+    item = Item()
+    item.text = request.POST.get('item_text','')
+    item.save()
     return render(request, 'home.html',{
         'new_item_text':request.POST.get('item_text',''),
     })
