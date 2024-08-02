ad21846a2b457e08d3c9fdeab3d3bb44270ab796
akduang <2019364641@qq.com>
2024-08-02 11:50:40 +0800

    model for list items and associated migrations

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 # from django.http import HttpResponse
 # from django.template.loader import render_to_string
 # from lists.views import home_page
-
+from lists.models import Item
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
@@ -27,3 +27,18 @@ class HomePageTest(TestCase):
         # self.assertIn('<title>To-Do lists</title>',html)
         # self.assertTrue(html.endswith('</html>'))
         # self.assertTemplateUsed(response, 'wrong.html')
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+        seconed_item = Item()
+        seconed_item.text = 'Item the second'
+        seconed_item.save()
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        seconed_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(seconed_saved_item.text, 'Item the second')
